8ee01d5636915af7409d40761cabcbd190765872
Kola <kolapo.abolarinwa@slimmingworld.co.uk>
2017-05-30 16:10:40 +0100

    Moved functions to a library file
    Moved the basic functions of the calculator to Calculator.fs

--- SampleCalculator/Calculator.fs
+++ SampleCalculator/Calculator.fs
@@ -1,4 +1,26 @@
 ﻿namespace SampleCalculator
 
-type Class1() = 
-    member this.X = "F#"
+module Maths=
+
+    module Add =
+        /// Adds two numbers together.
+        let add x y = y + x
+
+    module Subtract =
+        /// Subtracts one number from another.
+        let subtract x y = y - x
+
+    module Multiple =
+        /// Multiplies two numbers together.
+        let times x y = x * y
+    module Divide =
+        /// Divides one number by another. Only works for whole numbers!
+        let divideBy (x:int) (y:int) = y / x
+
+    module Double =
+        /// Doubles a number.
+        let double = Multiple.times 2
+
+    module Half =
+        /// Halves a number.
+        let halve = Divide.divideBy 2
--- SampleCalculator/Script.fsx
+++ SampleCalculator/Script.fsx
@@ -4,29 +4,40 @@
 #load "Calculator.fs"
 open SampleCalculator
 
-(* Start by filling in the following functions.
-   Add has been done for you. Fill in the implementation
-   for the others! *)
+open Maths.Multiple
+open Maths.Add
+open Maths.Divide
+open Maths.Double
+open Maths.Half
+open Maths.Subtract
 
-/// Adds two numbers together.
-let add x y = y + x
-/// Multiplies two numbers together.
-let times x y = x * y
 
-// watch out for ordering on these functions...
-/// Subtracts one number from another.
-let subtract x y = y - x
-/// Divides one number by another. Only works for whole numbers!
-let divideBy (x:int) (y:int) = y / x
-// see if you can reuse the functions defined above here
-/// Doubles a number.
-let double = times 2
-/// Halves a number.
-let halve = divideBy 2
+//(* Start by filling in the following functions.
+//   Add has been done for you. Fill in the implementation
+//   for the others! *)
 
-// compose "add" and "double" here using the >> operator (and remove the explicit "number" argument)
-/// Adds five to a number and then doubles it.
+///// Adds two numbers together.
+//let add x y = y + x
+
+///// Multiplies two numbers together.
+//let times x y = x * y
+
+//// watch out for ordering on these functions...
+///// Subtracts one number from another.
+//let subtract x y = y - x
+///// Divides one number by another. Only works for whole numbers!
+//let divideBy (x:int) (y:int) = y / x
+//// see if you can reuse the functions defined above here
+///// Doubles a number.
+//let double = times 2
+///// Halves a number.
+//let halve = divideBy 2
+
+//// compose "add" and "double" here using the >> operator (and remove the explicit "number" argument)
+///// Adds five to a number and then doubles it.
 let addFiveAndDouble = add 5 >> double
+
+
 
 // test out your code with these samples: -
 let simpleAdd = add 5 10 // should = 15
