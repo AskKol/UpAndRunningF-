6de3b751f4ee304fb0bd0d10b59e800cc04d8471
Kola <kolapo.abolarinwa@slimmingworld.co.uk>
2017-06-01 16:00:06 +0100

    CompletedSampleCalculator
    Completed the sample calculator application

---
+++ SampleCalculator/ImprovedCalculatorWithRecordScript.fsx
@@ -0,0 +1,6 @@
+﻿#load "CalculatorWithRecordHistory.fs"
+open SampleCalculator.RMaths
+
+let addFiveAndDouble = Add 5.0 >> Double
+
+let simplePipeline = { Answer = 0.0; History = ResizeArray() } |> Add 5.0 |> Multiple 3.0 |> Subtract 5.0 |> Division 2.0 |> addFiveAndDouble |> printf "%A"
