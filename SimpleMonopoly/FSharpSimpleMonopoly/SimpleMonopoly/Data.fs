dd15ff4b95ee21366b4cb54c464613da2327bae1
Kola <kolapo.abolarinwa@slimmingworld.co.uk>
2017-06-02 10:53:35 +0100

    Starting up monopoly

--- SimpleMonopoly/FSharpSimpleMonopoly/SimpleMonopoly/Data.fs
+++ SimpleMonopoly/FSharpSimpleMonopoly/SimpleMonopoly/Data.fs
@@ -1,25 +1,25 @@
-﻿namespace SimpleMonopoly
-
-module Monopoly.Data
-
-let Board =
-    [ Go;
-      Property "Old Kent Road"; CommunityChest 1; Property "Whitechapel Road";
-      Tax "Income Tax"; Station "Kings Cross";
-      Property "Angel Islington"; Chance 1; Property "Euston Road"; Property "Pentonville Road";
-      Jail;
+﻿   namespace SimpleMonopoly
+   open SimpleMonopoly.Types
+   module Data =
+   
+        let Board =
+            [ Go;
+              Property "Old Kent Road"; CommunityChest 1; Property "Whitechapel Road";
+              Tax "Income Tax"; Station "Kings Cross";
+              Property "Angel Islington"; Chance 1; Property "Euston Road"; Property "Pentonville Road";
+              Jail;
        
-      Property "Pall Mall"; Utility "Electric Company"; Property "Whitehall"; Property "Northumberland Avenue";
-      Station "Marylebone Road";
-      Property "Bow Street"; CommunityChest 2; Property "Marlborough Street"; Property "Vine Street";
-      FreeParking;
+              Property "Pall Mall"; Utility "Electric Company"; Property "Whitehall"; Property "Northumberland Avenue";
+              Station "Marylebone Road";
+              Property "Bow Street"; CommunityChest 2; Property "Marlborough Street"; Property "Vine Street";
+              FreeParking;
        
-      Property "Strand"; Chance 2; Property "Fleet Street"; Property "Trafalgar Square";
-      Station "Fenchurch Street";
-      Property "Leicester Square"; Property "Coventry Street"; Utility "Water Works"; Property "Piccadilly";
-      GoToJail;
+              Property "Strand"; Chance 2; Property "Fleet Street"; Property "Trafalgar Square";
+              Station "Fenchurch Street";
+              Property "Leicester Square"; Property "Coventry Street"; Utility "Water Works"; Property "Piccadilly";
+              GoToJail;
        
-      Property "Regent Street"; Property "Oxford Street"; CommunityChest 3; Property "Bond Street";
-      Station "Liverpool Street"; Chance 3;
-      Property "Park Lane"; Tax "Super Tax"; Property "Mayfair"; ]
+              Property "Regent Street"; Property "Oxford Street"; CommunityChest 3; Property "Bond Street";
+              Station "Liverpool Street"; Chance 3;
+              Property "Park Lane"; Tax "Super Tax"; Property "Mayfair"; ]
 
--- SimpleMonopoly/FSharpSimpleMonopoly/SimpleMonopoly/Types.fs
+++ SimpleMonopoly/FSharpSimpleMonopoly/SimpleMonopoly/Types.fs
@@ -1,25 +1,27 @@
-﻿namespace Monopoly.Types
+﻿namespace SimpleMonopoly
+    module Types=
 
-type Position = 
-    | Property of Name : string
-    | Station of Name : string
-    | Utility of Type : string
-    | Chance of Id : int // a way of uniquely identifying the different Chance positions on the board
-    | CommunityChest of Id : int 
-    | Tax of Type : string
-    | GoToJail
-    | Go
-    | FreeParking
-    | Jail
+        type Position = 
+            | Property of Name : string
+            | Station of Name : string
+            | Utility of Type : string
+            | Chance of Id : int // a way of uniquely identifying the different Chance positions on the board
+            | CommunityChest of Id : int 
+            | Tax of Type : string
+            | GoToJail
+            | Go
+            | FreeParking
+            | Jail
 
-type Outcome = 
-    | Pay of int
-    | Earn of int
-    | Bankrupt
+        type Outcome = 
+            | Pay of int
+            | Earn of int
+            | Bankrupt
 
-type Player = 
-    { Name : string
-      Position : Position
-      Money : int }
+        type Player = 
+            { Name : string
+              Position : Position
+              Money : int }
 
 
+  
